270c97980918beeae1a45e52dbc20abbeb9638b5
soo-hail <mohammed01705@gmail.com>
2024-12-11 14:09:05 +0530

    13:03

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation 
+
 # NOTE:
 # dataclass is used to avoid writing boilerplate code, it automatically generates common methods like __init__, __repr__, and __eq__. 
 
@@ -63,4 +66,8 @@ if __name__ == '__main__':
     obj = DataIngestion()
 
     # Call the data ingestion method
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path = obj.initiate_data_ingestion()
+    
+    # Data Transformation.
+    data_tranformation = DataTransformation()
+    data_tranformation.initiate_data_transformation(train_path=train_data_path, test_path=test_data_path)
